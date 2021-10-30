3048de639e7b5082f8cf54a7db672db2eea3cf55
ShubhamB2595 <shubhambhele95@gmail.com>
2021-10-30 20:22:22 +0530

    UC2 - ability to peek and pop from stack

--- src/com/stack/Stack.java
+++ src/com/stack/Stack.java
@@ -1,6 +1,5 @@
 package com.stack;
 
-
 public class Stack {
 
 	Node top = null;
@@ -41,6 +40,36 @@ public class Stack {
 		while(temp != null) {
 			System.out.println(temp.data);
 			temp = temp.next;
+		}
+	}
+	
+	//method pop for deleting data from stack
+	void pop() {
+				
+		if(top == null) {
+			System.out.println("Stack is Empty");
+		}
+		else {
+			top = top.next;
+		}
+	}
+	
+	//method pop for deleting data from stack
+	int peek() {
+							
+		if(top == null) {
+			System.out.println("Stack is Empty");
+			return 0;
+		}
+		else {
+			Node temp = top;
+			Node temp1 = temp.next;
+			
+			while(temp1.next != null) {
+				temp = temp1;
+				temp1 = temp1.next;
+			}
+			return temp1.data;
 		}
 	}
 	
--- src/com/stack/StackMain.java
+++ src/com/stack/StackMain.java
@@ -7,11 +7,21 @@ public class StackMain {
 		//System.out.println("Welcome to Stack program");
 		Stack mystack = new Stack();
 		
+		//Adding data to stack
 		mystack.push(70);
 		mystack.push(30);
 		mystack.push(56);
+		
+		//printing all elements of stack
 		mystack.display();
 
+		//printing last element of stack
+		int last = mystack.peek();
+		System.out.println("Last element of stack: " + last);
+		
+		//deleting 1st element from stack
+		mystack.pop();
+		mystack.display();
 	}
 
 }
